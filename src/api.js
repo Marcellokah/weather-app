96e45577bb726d929ba1bfbe6e20894547a90ba8
Marcellokah <aalma47@gmail.com>
2022-11-15 18:09:33 +0100

    Search component's done

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'a448e9ee72mshe6f1320a35c53e6p17dd2fjsnb4027384a912',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
